d754c86212f78da591f861bccf9be00cf60602a4
Weiran Guo <weiranguo@icloud.com>
2017-10-28 17:10:39 -0700

    finish part 4

--- proj1/UDPPingerClient.py
+++ proj1/UDPPingerClient.py
@@ -3,16 +3,29 @@
 
 from socket import *
 import os
+import signal
+import time
 
 serverName = 'localhost'
 serverPort = 12000
 
 # Create a UDP socket
 # Notice the use of SOCK_DGRAM for UDP packets
+
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.settimeout(1)
 message = input('Input lowercase sentence: ')
-clientSocket.sendto(str.encode(message), (serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(1024)
-print(bytes.decode(modifiedMessage))
+#Try 10 times
+for i in range(1,10):
+
+    timeStart = time.clock()
+    try:
+        clientSocket.sendto(str.encode(message), (serverName, serverPort))
+        modifiedMessage, serverAddress = clientSocket.recvfrom(1024)
+        print("Server says:", bytes.decode(modifiedMessage))
+        RTT = (time.clock() - timeStart) * 1000
+    except:
+        print("Request time out")
+ 
 
 clientSocket.close()
