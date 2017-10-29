97b218965c4e4ef778785ae135734a8563584b63
Weiran Guo <weiranguo@icloud.com>
2017-10-28 18:08:54 -0700

    finish part 7

--- proj1/UDPPingerClient.py
+++ proj1/UDPPingerClient.py
@@ -3,6 +3,7 @@
 
 from socket import *
 import os
+import datetime
 import signal
 import time
 
@@ -11,21 +12,36 @@ serverPort = 12000
 
 # Create a UDP socket
 # Notice the use of SOCK_DGRAM for UDP packets
-
+def abbrev(x):
+    return{
+        1: 'M',
+        2: 'T',
+        3: 'W',
+        4: 'R',
+        5: 'F',
+        6: 'S',
+        7: 'U'}.get(x)
+        
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.settimeout(1)
-message = input('Input lowercase sentence: ')
+message = 'abc'
+
 #Try 10 times
 for i in range(1,10):
-
     timeStart = time.clock()
+    start = datetime.datetime.now()
     try:
         clientSocket.sendto(str.encode(message), (serverName, serverPort))
         modifiedMessage, serverAddress = clientSocket.recvfrom(1024)
-        print("Server says:", bytes.decode(modifiedMessage))
+        end = datetime.datetime.now()
         RTT = (time.clock() - timeStart) * 1000
+        print("Ping", i, start.strftime("%Y-%m-%d"),
+              abbrev(start.weekday()),start.strftime("%H:%M"), "PCT")
+        print("Ping", i, end.strftime("%Y-%m-%d"),
+              abbrev(end.weekday()), start.strftime("%H:%M"), "PCT")
+        print("RTT: %.3f\n" % RTT)
     except:
-        print("Request time out")
+        print("Request time out\n")
  
 
 clientSocket.close()
